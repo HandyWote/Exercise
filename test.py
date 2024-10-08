4bbf80b38e9ea10a7c5d273fd11b1b32e15f89e4
Handy_Wote <18666119673@163.com>
2024-10-08 23:15:50 +0800

    LanQiao mengxin

--- test.py
+++ test.py
@@ -1,36 +1,21 @@
-s = input().replace('-', '')  # 移除横杠
-a = ''
-for i in s:
-    if i != 'X':  # 如果不是'X'，则直接添加到字符串a中
-        a += i
-    else:  # 如果是'X'，则添加字符'X'到字符串a中
-        a += 'X'
+def count_different_amounts(coins):
+    n = len(coins)
+    amounts = set()  # 使用集合来存储不同的金额，避免重复
 
-# 获取最后一位字符
-last_char = s[-1]
+    # 考虑使用0枚、1枚、2枚和3枚硬币的情况
+    for i in range(n + 1):
+        for j in range(n + 1):
+            for k in range(n + 1):
+                if i + j + k <= 3:
+                    amount = coins[i-1] + coins[j-1] + coins[k-1]
+                    if 0 <= i-1 < n and 0 <= j-1 < n and 0 <= k-1 < n:
+                        amounts.add(amount if amount > 0 else 0)
 
-# 计算加权和
-j = 0
-for i in range(9):
-    j += int(a[i]) * (10 - i)
+    return len(amounts) + 1  # 加1是为了包括0元的情况
 
-# 计算包括最后一位的加权和
-if last_char == 'X':
-    j += 10 * 10  # 如果最后一位是'X'，则乘以10
-else:
-    j += int(last_char) * 1  # 否则，乘以1
+# 读取输入
+n = int(input())
+coins = list(map(int, input().split()))
 
-# 计算模11的余数
-j = j % 11
-
-# 如果余数为10，将余数设置为'X'
-if j == 10:
-    j = 'X'
-
-# 比较计算出的余数和最后一位字符
-if j == last_char:
-    print('Right')
-else:
-    # 如果不一致，打印修正后的ISBN号
-    corrected_isbn = s[:-1] + str(j)
-    print(corrected_isbn)
+# 计算并输出结果
+print(count_different_amounts(coins))
