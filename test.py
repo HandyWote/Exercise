77cfc33e474da6e6c6c11d2946af28e00be2579c
HandyC <18666119673@163.com>
2024-10-10 23:40:14 +0800

    10.10

--- test.py
+++ test.py
@@ -1,4 +1,29 @@
-n = int(input())
-a = list(map(int, input().split()))
-a.sort()
-print(' '.join(map(str, a)))
+n,m,k = map(int,input().split())
+g = []
+for i in range(n):
+    g.append(list(map(int,input().split())))
+r = []
+for i in range(n):
+    for j in range(m):
+        if g[i][j]:
+            r.append([g[i][j],i + 1,j + 1])
+r.sort(reverse=True)
+ans = 0
+p = [-1,-1]
+time = 0
+for x,i,j in r:
+    if p == [-1,-1]:
+        if time + 2 * i + 1 <= k:
+            ans += x
+            p = [i,j]
+            time += i + 1
+        else:
+            break
+    else:
+        if time + abs(p[0] - i) + abs(p[1] - j) + i + 1<= k:
+            ans += x
+            time += abs(p[0] - i) + abs(p[1] - j) + 1
+            p = [i,j]
+        else:
+            break
+print(ans)
