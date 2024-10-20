47bd83ab25346acb4a1c0cb30a7458546c661021
HandyC <18666119673@163.com>
2024-10-20 23:35:37 +0800

    10.20

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,14 +1,6 @@
-from collections import Counter
-n = int(input())
-vo = []
-for _ in range (n):
-    vo.append(input())
-    vo.append(vo[-1][0]+vo[-1][-1])
-    vo.remove(vo[-2])
-ans = 2
-
-
-
-
-
-print()
+m = [int(i) for i in range(1,2025)]
+m1 =[]
+for i in range(len(m)):
+    for j in range(i,len(m)):
+        m1.append(m[i]+m[j])
+print(sort)
