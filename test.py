5fec4c789917d17cf3a024615d2fd9e16a7b58f5
HandyC <18666119673@163.com>
2024-10-26 20:13:03 +0800

    10.26

--- test.py
+++ test.py
@@ -1,29 +1,15 @@
-n,m,k = map(int,input().split())
-g = []
-for i in range(n):
-    g.append(list(map(int,input().split())))
-r = []
-for i in range(n):
-    for j in range(m):
-        if g[i][j]:
-            r.append([g[i][j],i + 1,j + 1])
-r.sort(reverse=True)
-ans = 0
-p = [-1,-1]
-time = 0
-for x,i,j in r:
-    if p == [-1,-1]:
-        if time + 2 * i + 1 <= k:
-            ans += x
-            p = [i,j]
-            time += i + 1
-        else:
-            break
-    else:
-        if time + abs(p[0] - i) + abs(p[1] - j) + i + 1<= k:
-            ans += x
-            time += abs(p[0] - i) + abs(p[1] - j) + 1
-            p = [i,j]
-        else:
-            break
-print(ans)
+from collections import Counter
+
+# 假设我们有一个字符串列表
+elements = ['apple', 'banana', 'apple', 'orange', 'banana', 'apple']
+
+# 使用Counter来计数
+counter = Counter(elements)
+
+# 定义一个自定义的顺序
+custom_order = ['orange', 'banana', 'apple']
+
+# 根据自定义顺序排序并打印
+for element in custom_order:
+    if element in counter:
+        print(f"{element}: {counter[element]}")
